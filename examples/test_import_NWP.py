8bb78823d21c7056c66a33c374562702a2504684
Wout Dewettinck <wout.dewettinck@ugent.be>
2021-08-17 14:52:43 +0200

    First error-free version of reprojection

--- examples/test_import_NWP.py
+++ examples/test_import_NWP.py
@@ -1,12 +1,13 @@
-import netCDF4
-from pysteps.io.importers import import_bom_rf3_xr, import_bom_rf3
+from pysteps.io.importers import import_bom_rf3_xr
 from pysteps.visualization import plot_precip_field
 import matplotlib.pyplot as plt
 from pprint import pprint
 import xarray as xr
 from pysteps.utils.conversion import to_rainrate
 import numpy as np
-from pysteps.visualization.utils import reproject_geodata
+from pysteps.visualization.utils import reproject_geodata  # ???
+import rasterio
+import netCDF4
 
 """
 ncf = netCDF4.Dataset("C:/Users/woutd/Documents/School/UGent/Master/Stage/Pysteps/pysteps_data/20201031_0000_regrid_short.nc")
@@ -20,7 +21,27 @@ for i in range(R.shape[0]):
     plt.close()
 """
 
-R, _, metadata = import_bom_rf3_xr("C:/Users/woutd/Documents/School/UGent/Master/Stage/Pysteps/pysteps_data/20201031_0000_regrid_short.nc", varname="accum_prcp", legacy=True)
+# R, _, metadata = import_bom_rf3_xr("C:/Users/woutd/Documents/School/UGent/Master/Stage/Pysteps/pysteps_data/20201031_0000_regrid_short.nc", varname="accum_prcp", legacy=True)
+
+R = import_bom_rf3_xr(
+    "/home/wdewettin/Downloads/20201031_0000_regrid_short.nc",
+    varname="accum_prcp",
+    legacy=False,
+)
+R = xr.DataArray.squeeze(R)
+
+# Removing this makes an error go away
+del R.attrs["transform"]
+
+R.isel(time=30).plot()
+R.rio.set_crs(R.attrs["projection"])
+plt.savefig("Original.png")
+plt.close()
+
+R_new = R.rio.reproject("EPSG:2990", nodata=-10)
+R_new.where(R_new != -10).isel(time=30).plot()
+plt.show()
+plt.close()
 
 """
 # Decumulate data
@@ -29,6 +50,7 @@ del R
 R = R_dec
 """
 
+"""
 pprint(metadata)
 # print(reproject_geodata(metadata))
 
@@ -40,18 +62,24 @@ metadata["projection"] = '+proj=longlat +datum=WGS84 +no_defs'
 plot_precip_field(R[0, 30, :, :], geodata=metadata)
 plt.show()
 plt.close()
+"""
 
 """
 metadata["accutime"] = 10
 
 R, metadata = to_rainrate(R, metadata)
 pprint(metadata)
+"""
 
-for i in range(R.shape[0]):
+"""
+R = np.squeeze(R)
+print(R.shape)
+
+for i in range(R.shape[0], 10):
+    print(i)
     plot_precip_field(R[i, :, :], geodata=metadata)
-    plt.savefig("RR_NWP_{}.png".format(i))
+    plt.savefig("NWP_{}.png".format(i))
     plt.close()
 """
 
-# R = import_bom_rf3_xr("C:/Users/woutd/Documents/School/UGent/Master/Stage/Pysteps/pysteps_data/radar/bom/
-# prcp-cscn/2/2018/06/16/2_20180616_100000.prcp-cscn.nc")
+# R = import_bom_rf3_xr("C:/Users/woutd/Documents/School/UGent/Master/Stage/Pysteps/pysteps_data/radar/bom/prcp-cscn/2/2018/06/16/2_20180616_100000.prcp-cscn.nc")
