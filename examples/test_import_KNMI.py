7af957eb61e4cb4a9faee70fd9a768fc7b5514c3
Wout Dewettinck <wout.dewettinck@ugent.be>
2021-08-18 15:14:37 +0200

    Plotting xarrays of radar data

--- examples/test_import_KNMI.py
+++ examples/test_import_KNMI.py
@@ -8,14 +8,23 @@ d = datetime.datetime(2018, 1, 5, 9, 0)
 
 for _ in range(133):
     time = d.time().strftime("%H%M")
-    R, _, metadata = import_knmi_hdf5(
+
+    R = import_knmi_hdf5(
         "/home/wdewettin/Downloads/KNMI/20180105/Bias_Corrected_Radar/RAD_NL25_RAP_5min_20180105{}.h5".format(
             time
-        ),
-        legacy=True,
+        )
     )
 
-    plot_precip_field(R, ptype="depth", units="mm", geodata=metadata)
+    geodata = {
+        "projection": R.attrs["projection"],
+        "x1": R.x.x1,
+        "y1": R.y.y1,
+        "x2": R.x.x2,
+        "y2": R.y.y2,
+        "yorigin": R.attrs["yorigin"],
+    }
+
+    plot_precip_field(R, ptype="depth", units="mm", geodata=geodata)
     plt.savefig("KNMI_radar_{}.png".format(time))
     plt.close()
 
