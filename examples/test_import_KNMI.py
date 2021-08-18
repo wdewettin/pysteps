c61e0d9ef3f47d121b5bdecf5a3f7a63179200ce
Wout Dewettinck <wout.dewettinck@ugent.be>
2021-08-18 15:04:21 +0200

    Reading and plotting KNMI radar data

---
+++ examples/test_import_KNMI.py
@@ -0,0 +1,24 @@
+from pysteps.io import import_knmi_hdf5
+from pprint import pprint
+from pysteps.visualization import plot_precip_field
+import matplotlib.pyplot as plt
+import datetime
+
+d = datetime.datetime(2018, 1, 5, 9, 0)
+
+for _ in range(133):
+    time = d.time().strftime("%H%M")
+    R, _, metadata = import_knmi_hdf5(
+        "/home/wdewettin/Downloads/KNMI/20180105/Bias_Corrected_Radar/RAD_NL25_RAP_5min_20180105{}.h5".format(
+            time
+        ),
+        legacy=True,
+    )
+
+    plot_precip_field(R, ptype="depth", units="mm", geodata=metadata)
+    plt.savefig("KNMI_radar_{}.png".format(time))
+    plt.close()
+
+    print("plotted " + time)
+
+    d = d + datetime.timedelta(minutes=5)
