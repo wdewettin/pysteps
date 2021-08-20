100bd96d16f7c5f5c5ad3c895a31d97c8b318f00
Wout Dewettinck <wout.dewettinck@ugent.be>
2021-08-20 18:18:21 +0200

    Further attempts to plot RMI NWP data

--- examples/RMI_reprojection.py
+++ examples/RMI_reprojection.py
@@ -1,6 +1,7 @@
-from pysteps.io import import_odim_hdf5
+from pysteps.io import import_odim_hdf5, import_rmi_nwp_xr
 from pysteps.visualization import plot_precip_field
 import matplotlib.pyplot as plt
+from pprint import pprint
 
 # Import radar data
 
@@ -20,6 +21,25 @@ geodata = {
 plt.figure(1)
 plt.title("Radar at 2021-07-04 00:00:00")
 plot_precip_field(R, geodata=geodata)
+
+# Import NWP data
+
+R_NWP = import_rmi_nwp_xr("ao13_2021071406_steps_be13_5min.nc")
+
+pprint(R_NWP)
+
+geodata_NWP = {
+    "projection": "proj=lcc lon_0=4.55 lat_1=50.8 lat_2=50.8 a=6371229 es=0 +x_0=365950 +y_0=-365950",  # R_NWP.attrs["projection"], # "proj=lcc lon_0=4.55 lat_1=50.8 lat_2=50.8 a=6371229 es=0 +x_0=365950 +y_0=-365950"
+    "x1": R_NWP.x.x1 / 1000.0,
+    "y1": -R_NWP.y.y2 / 1000.0,
+    "x2": R_NWP.x.x2 / 1000.0,
+    "y2": -R_NWP.y.y1 / 1000.0,
+    "yorigin": R_NWP.attrs["yorigin"],
+}
+
+plt.figure(2)
+plt.title("NWP at 2021-07-14 10:05:00")
+plot_precip_field(R_NWP[0, :, :], geodata=geodata_NWP)
 
 # Show all the figures
 
