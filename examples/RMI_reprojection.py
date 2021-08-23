c64bb13732b5f0d27159534cda7c92539e5b4caa
Wout Dewettinck <wout.dewettinck@ugent.be>
2021-08-23 13:55:03 +0200

    Tried to plot the interpolated NWP data

--- examples/RMI_reprojection.py
+++ examples/RMI_reprojection.py
@@ -10,6 +10,8 @@ R = import_odim_hdf5(
     "./rainbow/20210704/20210704160500.rad.becomp00.image.rate.beborder00_comp_sri.hdf"
 )
 
+pprint(R.x)
+
 geodata = {
     "projection": R.attrs["projection"],
     "x1": R.x.x1,
@@ -19,14 +21,15 @@ geodata = {
     "yorigin": R.attrs["yorigin"],
 }
 
+pprint(geodata)
+
 # Plot radar data
 
 plt.figure(1)
 plot_precip_field(R, geodata=geodata)
 plt.title("Radar at 2021-07-04 16:05:00")
 
-
-# Import NWP data
+# Import native NWP data
 
 R_NWP = import_rmi_nwp_xr("./nwp/ao13_2021070412_native_5min.nc")
 
@@ -39,15 +42,42 @@ geodata_NWP = {
     "yorigin": R_NWP.attrs["yorigin"],
 }
 
+pprint(geodata_NWP)
+
 R_NWP.attrs["accutime"] = 5.0
 R_NWP[:], R_NWP.attrs = to_rainrate(R_NWP[:], R_NWP.attrs)
 
-pprint(R_NWP.attrs)
+# Plot native NWP data
 
 plt.figure(2)
 plot_precip_field(R_NWP[0, :, :], geodata=geodata_NWP)
 plt.title("NWP at 2021-07-04 16:05:00")
 
+# Import reprojected NWP data
+
+R_NWP_radar = import_rmi_nwp_xr("./nwp/ao13_2021070412_radar512_5min.nc")
+
+pprint(R_NWP_radar)
+
+geodata_NWP_radar = {
+    "projection": R_NWP_radar.attrs["projection"],
+    "x1": R_NWP_radar.x.x1,
+    "y1": R_NWP_radar.y.y1,
+    "x2": R_NWP_radar.x.x2,
+    "y2": R_NWP_radar.y.y2,
+    "yorigin": R_NWP_radar.attrs["yorigin"],
+}
+
+pprint(geodata_NWP_radar)
+
+R_NWP_radar.attrs["accutime"] = 5.0
+R_NWP_radar[:], R_NWP_radar.attrs = to_rainrate(R_NWP_radar[:], R_NWP_radar.attrs)
+
+# Plot reprojected NWP data
+
+plt.figure(3)
+plot_precip_field(R_NWP_radar[0, :, :], geodata=geodata_NWP_radar)
+plt.title("reprojected NWP (by Lesley) at 2021-07-04 16:05:00")
 
 # Show all plots
 
