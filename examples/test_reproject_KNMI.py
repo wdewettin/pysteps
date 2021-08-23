043d71adfecc7a8a2727888835b6966aaa194410
Wout Dewettinck <wout.dewettinck@ugent.be>
2021-08-23 21:09:29 +0200

    Small changes to test file

--- examples/test_reproject_KNMI.py
+++ examples/test_reproject_KNMI.py
@@ -24,6 +24,8 @@ R_NWP = R_NWP[:, ::-1, :]  # Reverse the order of the rows
 time = ncf.variables["time"][:]
 x = ncf.variables["x"]
 y = ncf.variables["y"]
+pprint(x[:])
+pprint(y[:])
 projection = ncf.variables["crs"].proj4_params
 
 geodata_NWP = {
@@ -42,11 +44,11 @@ valid_time = datetime.datetime(1970, 1, 1, 0, 0, 0) + datetime.timedelta(
 )
 valid_time = valid_time.strftime("%Y%m%d_%H%M")
 
+plt.figure(1)
 plot_precip_field(R_NWP[i, :, :], ptype="depth", units="mm", geodata=geodata_NWP)
 print("plotted NWP " + valid_time)
 plt.title("Original NWP data")
-plt.savefig("D.png")
-plt.close()
+plt.show()
 
 # 1.B Setting NWP data grid info
 
@@ -101,11 +103,11 @@ pprint(geodata)
 
 time = d.strftime("%Y%m%d_%H%M")
 
+plt.figure(2)
 plot_precip_field(R, ptype="depth", units="mm", geodata=geodata)
 print("plotted radar " + time)
 plt.title("Original radar data")
-plt.savefig("E.png")
-plt.close()
+plt.show()
 
 # 2.B Setting radar data grid info
 
@@ -116,41 +118,37 @@ radar_transform = A.translation(R.x.x1 - 0.5, R.y.y1 - 0.5)  # Moet die 0.5 ???
 radar_crs = geodata["projection"]
 radar_crs = rasterio.crs.CRS.to_dict(rasterio.crs.CRS.from_proj4(radar_crs))
 
-for a in range(20):
-    radar_crs["lat_0"] = (
-        90 - 0.1 ** a
-    )  # For some reason, it gives errors if lat_0 = 90.0
+# For some reason, it gives errors if lat_0 = 90.0
 
-    """
-    radar_crs["y_0"] = 300.0 # Change false northing
-    """
+"""
+radar_crs["y_0"] = 300.0 # Change false northing
+"""
 
-    """
-    # Poging met nieuwe projectiestring van Ruben!
-    radar_crs = ' +proj=stere +lat_0=90 +lat_ts=60 +lon_0=0 +x_0=0 +y_0=0 +a=6378.137 +rf=298.252840776255 +units=m +no_defs'
-    radar_crs = rasterio.crs.CRS.to_dict(rasterio.crs.CRS.from_proj4(radar_crs))
-    radar_crs["lat_0"] = 89.99999999  # For some reason, it gives errors if lat_0 = 90.0
-    """
+"""
+# Poging met nieuwe projectiestring van Ruben!
+radar_crs = ' +proj=stere +lat_0=90 +lat_ts=60 +lon_0=0 +x_0=0 +y_0=0 +a=6378.137 +rf=298.252840776255 +units=m +no_defs'
+radar_crs = rasterio.crs.CRS.to_dict(rasterio.crs.CRS.from_proj4(radar_crs))
+radar_crs["lat_0"] = 89.99999999  # For some reason, it gives errors if lat_0 = 90.0
+"""
 
-    print("radar_crs = {}".format(radar_crs))
+print("radar_crs = {}".format(radar_crs))
 
-    # 3. Reprojecting radar data
+# 3. Reprojecting radar data
 
-    R_NWP_rprj = np.zeros_like(R)
+R_NWP_rprj = np.zeros_like(R)
 
-    reproject(
-        R_NWP[i, :, :],
-        R_NWP_rprj,
-        src_transform=NWP_transform,
-        src_crs=NWP_crs,
-        dst_transform=radar_transform,
-        dst_crs=radar_crs,
-        resampling=Resampling.nearest,
-        dst_nodata=np.nan,
-    )
+reproject(
+    R_NWP[i, :, :],
+    R_NWP_rprj,
+    src_transform=NWP_transform,
+    src_crs=NWP_crs,
+    dst_transform=radar_transform,
+    dst_crs=radar_crs,
+    resampling=Resampling.nearest,
+    dst_nodata=np.nan,
+)
 
-    plot_precip_field(R_NWP_rprj, ptype="depth", units="mm", geodata=geodata)
-    plt.title("Reprojected NWP data with lat_0 = {}".format(radar_crs["lat_0"]))
-    plt.savefig("F_{}.png".format(a))
-
-    plt.close()
+plt.figure(3)
+plot_precip_field(R_NWP_rprj, ptype="depth", units="mm", geodata=geodata)
+plt.title("Reprojected NWP data with lat_0 = {}".format(radar_crs["lat_0"]))
+plt.show()
