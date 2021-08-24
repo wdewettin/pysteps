b28b13137f7ac6abd30c937adce9d18449446c8f
Wout Dewettinck <wout.dewettinck@ugent.be>
2021-08-24 20:25:21 +0200

    Finished all plotting

--- wout/plot_presentation.py
+++ wout/plot_presentation.py
@@ -6,143 +6,14 @@ import os
 from datetime import datetime, timedelta
 from pprint import pprint
 from pysteps.cascade.bandpass_filters import filter_gaussian
-from pysteps import io, rcparams
+from pysteps import io, rcparams, verification
 from pysteps.cascade.decomposition import decomposition_fft
-from pysteps.utils import conversion, transformation
+from pysteps.utils import conversion, transformation, dimension
 from pysteps.visualization import plot_precip_field
 from pysteps.io import import_rmi_nwp_xr
 from pysteps.blending.utils import reprojection
-
-# Input
-date_str = "202107141000"  # input("Give date and time (e.g.: 201609281600):    ")
-n_timesteps = 36
-start_blending = 60
-end_blending = 120
-
-# 1. Plot radar fields
-
-# Convert date to string
-date = datetime.strptime(date_str, "%Y%m%d%H%M")
-
-# Import the data source
-data_source = rcparams.data_sources["rmi"]
-
-# Import the radar composite
-root_path = data_source["root_path"]
-path_fmt = data_source["path_fmt"]
-fn_pattern = data_source["fn_pattern"]
-fn_ext = data_source["fn_ext"]
-timestep = data_source["timestep"]
-importer_kwargs = data_source["importer_kwargs"]
-
-# for _ in range(168):
-
-filename = os.path.join(
-    root_path,
-    datetime.strftime(date, path_fmt),
-    datetime.strftime(date, fn_pattern) + "." + fn_ext,
-)
-
-R_radar = io.import_odim_hdf5(filename, "RATE", **importer_kwargs)
-
-# Convert to rain rate
-R_radar.attrs["accutime"] = 5.0
-R_radar[:], R_radar.attrs = conversion.to_rainrate(R_radar[:], R_radar.attrs)
-
-# Extract geodata to plot
-
-geodata_radar = {
-    "projection": R_radar.attrs["projection"],
-    "x1": R_radar.x.x1,
-    "y1": R_radar.y.y1,
-    "x2": R_radar.x.x2,
-    "y2": R_radar.y.y2,
-    "yorigin": R_radar.attrs["yorigin"],
-}
-
-"""
-# Plot the rainfall field
-plot_precip_field(R_radar, geodata=geodata_radar)
-plt.title("Radar image at {}".format(datetime.strftime(date, "%Y-%m-%d %H:%M:%S")))
-plt.savefig("./images/1_radar_image_{}.png".format(datetime.strftime(date, "%Y%m%d%H%M%S")))
-plt.close()
-"""
-
-# Plot the rainfall field
-plt.figure(1)
-plot_precip_field(R_radar, geodata=geodata_radar)
-plt.title("Radar image at {}".format(datetime.strftime(date, "%Y-%m-%d %H:%M:%S")))
-
-# date = date + timedelta(minutes=5)
-
-# Import native NWP data
-nwp_date_str = "202107140600"
-nwp_date = datetime.strptime(nwp_date_str, "%Y%m%d%H%M")
-R_NWP = import_rmi_nwp_xr(
-    "./nwp/ao13_{}_native_5min.nc".format(datetime.strftime(nwp_date, "%Y%m%d%H"))
-)
-
-xpixelsize_NWP = R_NWP.attrs["xpixelsize"]
-ypixelsize_NWP = R_NWP.attrs["ypixelsize"]
-
-x_NWP = R_NWP.x
-y_NWP = R_NWP.y
-
-R_NWP.attrs["projection"] = R_NWP.attrs["projection"] + " lat_0=50.8"
-
-geodata_NWP = {
-    "projection": R_NWP.attrs["projection"],
-    "x1": R_NWP.x.x1,
-    "y1": R_NWP.y.y1,
-    "x2": R_NWP.x.x2,
-    "y2": R_NWP.y.y2,
-    "yorigin": R_NWP.attrs["yorigin"],
-}
-
-R_NWP.attrs["accutime"] = 5.0
-R_NWP[:], R_NWP.attrs = conversion.to_rainrate(R_NWP[:], R_NWP.attrs)
-
-t0_str = "202107141005"
-t0 = datetime.strptime(t0_str, "%Y%m%d%H%M")
-
-# Plot reprojected NWP data
-R_NWP_rprj = reprojection(R_NWP[0, :, :], R_radar)
-
-plt.figure(2)
-plot_precip_field(R_NWP[0, :, :], geodata=geodata_NWP)
-plt.title("NWP data at {}".format(datetime.strftime(t0, "%Y-%m-%d %H:%M:%S")))
-
-# Plot reprojected NWP data
-plt.figure(3)
-plot_precip_field(R_NWP_rprj, geodata=geodata_radar)
-plt.title(
-    "Reprojected NWP data at {}".format(datetime.strftime(t0, "%Y-%m-%d %H:%M:%S"))
-)
-
-plt.close()
-
-"""
-for i in range(R_NWP.shape[0]):
-    # Plot native NWP data
-    t = t0 + i * timedelta(minutes=5)
-    print(t)
-
-    plot_precip_field(R_NWP[i, :, :], geodata=geodata_NWP)
-    plt.title("NWP data at {}".format(datetime.strftime(t, "%Y-%m-%d %H:%M:%S")))
-    plt.savefig("./images/2_nwp_data_{}.png".format(datetime.strftime(t, "%Y%m%d%H%M%S")))
-    plt.close()
-
-    # Reproject NWP data onto radar domain
-    R_NWP_rprj = reprojection(R_NWP[i, :, :], R_radar)
-
-    # Plot reprojected NWP data
-    plot_precip_field(R_NWP_rprj, geodata=geodata_radar)
-    plt.title("Reprojected NWP data at {}".format(datetime.strftime(t, "%Y-%m-%d %H:%M:%S")))
-    plt.savefig("./images/3_reprojected_nwp_data_{}.png".format(datetime.strftime(t, "%Y%m%d%H%M%S")))
-    plt.close()
-"""
-
-# Plot dummy NWP data
+from pysteps.nowcasts.linear_blending import forecast
+from pysteps.motion.lucaskanade import dense_lucaskanade
 
 
 def gaussian(x, max, mean, sigma):
@@ -170,10 +41,239 @@ def dummy_nwp(R, n_leadtimes, max=15, mean=3, sigma=0.25, speed=100):
     return R_nwp
 
 
-R_dummy = dummy_nwp(R_radar, n_timesteps)
+def linear_blending(
+    R_nowcast,
+    timesteps,
+    timestep,
+    R_nwp=None,
+    start_blending=120,
+    end_blending=240,
+    use_nwp=True,
+):
+    """This function the same as the forecast function from linear blending.
+    The only difference being that it is now a simple post-processing function,
+    taking the nowcast fields as input"""
 
-for i in range(n_timesteps):
-    t = date + timedelta(minutes=5) * (i + 1)
+    # Check if NWP data is given as input
+    if R_nwp is not None:
+
+        if len(R_nowcast.shape) == 4:
+            n_ens_members_nowcast = R_nowcast.shape[0]
+            if n_ens_members_nowcast == 1:
+                R_nowcast = np.squeeze(R_nowcast)
+        else:
+            n_ens_members_nowcast = 1
+
+        if len(R_nwp.shape) == 4:
+            n_ens_members_nwp = R_nwp.shape[0]
+            if n_ens_members_nwp == 1:
+                R_nwp = np.squeeze(R_nwp)
+        else:
+            n_ens_members_nwp = 1
+
+        n_ens_members_max = max(n_ens_members_nowcast, n_ens_members_nwp)
+        n_ens_members_min = min(n_ens_members_nowcast, n_ens_members_nwp)
+
+        if n_ens_members_min != n_ens_members_max:
+            if n_ens_members_nwp == 1:
+                R_nwp = np.repeat(R_nwp[np.newaxis, :, :], n_ens_members_max, axis=0)
+            elif n_ens_members_nowcast == 1:
+                R_nowcast = np.repeat(
+                    R_nowcast[np.newaxis, :, :], n_ens_members_max, axis=0
+                )
+            else:
+                repeats = [
+                    (n_ens_members_max + i) // n_ens_members_min
+                    for i in range(n_ens_members_min)
+                ]
+
+                if n_ens_members_nwp == n_ens_members_min:
+                    R_nwp = np.repeat(R_nwp, repeats, axis=0)
+                elif n_ens_members_nowcast == n_ens_members_min:
+                    R_nowcast = np.repeat(R_nowcast, repeats, axis=0)
+
+        # Check if dimensions are correct
+        assert (
+            R_nwp.shape == R_nowcast.shape
+        ), "The dimensions of R_nowcast and R_nwp need to be identical: dimension of R_nwp = {} and dimension of R_nowcast = {}".format(
+            R_nwp.shape, R_nowcast.shape
+        )
+
+        # Initialise output
+        R_blended = np.zeros_like(R_nowcast)
+
+        # Calculate the weights
+        for i in range(timesteps):
+            # Calculate what time we are at
+            t = (i + 1) * timestep
+
+            # Calculate the weight with a linear relation (weight_nwp at start_blending = 0.0)
+            # and (weight_nwp at end_blending = 1.0)
+            weight_nwp = (t - start_blending) / (end_blending - start_blending)
+
+            # Set weights at times before start_blending and after end_blending
+            if weight_nwp < 0.0:
+                weight_nwp = 0.0
+            elif weight_nwp > 1.0:
+                weight_nwp = 1.0
+
+            # Calculate weight_nowcast
+            weight_nowcast = 1.0 - weight_nwp
+
+            # Calculate output by combining R_nwp and R_nowcast,
+            # while distinguishing between ensemble and non-ensemble methods
+            if n_ens_members_max == 1:
+                R_blended[i, :, :] = (
+                    weight_nwp * R_nwp[i, :, :] + weight_nowcast * R_nowcast[i, :, :]
+                )
+            else:
+                R_blended[:, i, :, :] = (
+                    weight_nwp * R_nwp[:, i, :, :]
+                    + weight_nowcast * R_nowcast[:, i, :, :]
+                )
+
+            # Find where the NaN values are and replace them with NWP data
+            if use_nwp:
+                nan_indices = np.isnan(R_blended)
+                R_blended[nan_indices] = R_nwp[nan_indices]
+    else:
+        # If no NWP data is given, the blended field is simply equal to the nowcast field
+        R_blended = R_nowcast
+
+    return R_blended
+
+
+########################################################################
+# 0. Input
+date_str = "202107141100"  # input("Give date and time (e.g.: 201609281600):    ")
+n_timesteps = 36
+start_blending = 120
+end_blending = 240
+n_ens_members = 10
+
+########################################################################
+# 1. Plot radar fields
+
+# Convert date to string
+date = datetime.strptime(date_str, "%Y%m%d%H%M")
+
+# Import the data source
+data_source = rcparams.data_sources["rmi"]
+
+# Import the radar composite
+root_path = data_source["root_path"]
+path_fmt = data_source["path_fmt"]
+fn_pattern = data_source["fn_pattern"]
+fn_ext = data_source["fn_ext"]
+timestep = data_source["timestep"]
+importer_name = data_source["importer"]
+importer_kwargs = data_source["importer_kwargs"]
+
+# Find the radar files in the archive
+fns_radar = io.find_by_date(
+    date, root_path, path_fmt, fn_pattern, fn_ext, timestep, num_next_files=n_timesteps
+)
+
+# Read the data from the archive
+importer = io.get_method(importer_name, "importer")
+R_radar = io.read_timeseries(fns_radar, importer, **importer_kwargs)
+
+# Convert to rain rate
+R_radar.attrs["accutime"] = 5.0
+R_radar[:], R_radar.attrs = conversion.to_rainrate(R_radar[:], R_radar.attrs)
+
+# Extract geodata to plot
+geodata_radar = {
+    "projection": R_radar.attrs["projection"],
+    "x1": R_radar.x.x1,
+    "y1": R_radar.y.y1,
+    "x2": R_radar.x.x2,
+    "y2": R_radar.y.y2,
+    "yorigin": R_radar.attrs["yorigin"],
+}
+
+"""
+for i in range(n_timesteps + 1): 
+       
+    # Plot the radar rainfall field
+    t = date + i * timedelta(minutes=5)
+    print(t)
+    print(R_radar.t[i])
+    
+    plot_precip_field(R_radar[i, :, :], geodata=geodata_radar)
+    plt.title("Radar image at {}".format(datetime.strftime(t, "%Y-%m-%d %H:%M:%S")))
+    plt.savefig("./images/1_radar_image_{}.png".format(datetime.strftime(t, "%Y%m%d%H%M%S")))
+    plt.close()
+"""
+
+########################################################################
+# 2. Plot native NWP fields
+# 3. Plot reprojected NWP fields
+
+# Import native NWP data
+nwp_date_str = "202107140600"
+nwp_date = datetime.strptime(nwp_date_str, "%Y%m%d%H%M")
+R_NWP = import_rmi_nwp_xr(
+    "./nwp/ao13_{}_native_5min.nc".format(datetime.strftime(nwp_date, "%Y%m%d%H"))
+)
+
+# Modify geodata
+R_NWP.attrs["projection"] = R_NWP.attrs["projection"] + " lat_0=50.8"
+
+geodata_NWP = {
+    "projection": R_NWP.attrs["projection"],
+    "x1": R_NWP.x.x1,
+    "y1": R_NWP.y.y1,
+    "x2": R_NWP.x.x2,
+    "y2": R_NWP.y.y2,
+    "yorigin": R_NWP.attrs["yorigin"],
+}
+
+# Convert to rain rate
+R_NWP.attrs["accutime"] = 5.0
+R_NWP[:], R_NWP.attrs = conversion.to_rainrate(R_NWP[:], R_NWP.attrs)
+
+# Find start index corresponding with date
+t0_str = "202107141005"
+t0 = datetime.strptime(t0_str, "%Y%m%d%H%M")
+start_i = (date - t0) // timedelta(minutes=5)
+print("start_i = {}".format(start_i))
+
+# Initialise reprojection array
+R_NWP_rprj = np.zeros((n_timesteps + 1, R_radar.shape[1], R_radar.shape[2]))
+
+for i in range(start_i, start_i + n_timesteps + 1):
+    """
+    # Plot native NWP data
+    t = t0 + i * timedelta(minutes=5)
+    print(t)
+    print(R_NWP.t[i])
+
+    plot_precip_field(R_NWP[i, :, :], geodata=geodata_NWP)
+    plt.title("NWP data at {}".format(datetime.strftime(t, "%Y-%m-%d %H:%M:%S")))
+    plt.savefig("./images/2_nwp_data_{}.png".format(datetime.strftime(t, "%Y%m%d%H%M%S")))
+    plt.close()
+    """
+
+    # Reproject NWP data onto radar domain
+    R_NWP_rprj[i - start_i, :, :] = reprojection(R_NWP[i, :, :], R_radar[0, :, :])
+
+    """
+    # Plot reprojected NWP data
+    plot_precip_field(R_NWP_rprj[i - start_i, :, :], geodata=geodata_radar)
+    plt.title("Reprojected NWP data at {}".format(datetime.strftime(t, "%Y-%m-%d %H:%M:%S")))
+    plt.savefig("./images/3_reprojected_nwp_data_{}.png".format(datetime.strftime(t, "%Y%m%d%H%M%S")))
+    plt.close()
+    """
+
+########################################################################
+# 4. Plot dummy NWP data
+
+R_dummy = dummy_nwp(R_radar[0, :, :], n_timesteps + 1)
+
+"""
+for i in range(n_timesteps + 1):
+    t = date + timedelta(minutes=5) * i
     print(t)
 
     plot_precip_field(R_dummy[i, :, :], geodata=geodata_radar)
@@ -182,3 +282,213 @@ for i in range(n_timesteps):
         "./images/4_dummy_nwp_data_{}.png".format(datetime.strftime(t, "%Y%m%d%H%M%S"))
     )
     plt.close()
+"""
+
+########################################################################
+# 5. Calculate and plot STEPS nowcast
+
+# Find the radar files in the archive
+fns = io.find_by_date(
+    date, root_path, path_fmt, fn_pattern, fn_ext, timestep, num_prev_files=2
+)
+
+# Read the data from the archive
+importer = io.get_method(importer_name, "importer")
+R_input, _, metadata_input = io.read_timeseries(
+    fns, importer, legacy=True, **importer_kwargs
+)
+
+# Convert to rain rate
+metadata_input["accutime"] = 5.0
+R_input, metadata_input = conversion.to_rainrate(R_input, metadata_input)
+
+# Upscale data to 2 km to limit memory usage
+print(R_input.shape)
+R_input, metadata_input = dimension.aggregate_fields_space(
+    R_input, metadata_input, metadata_input["xpixelsize"] * 3
+)
+
+# Log-transform the data to unit of dBR, set the threshold to 0.1 mm/h,
+# set the fill value to -15 dBR
+R_input, metadata_input = transformation.dB_transform(
+    R_input, metadata_input, threshold=0.1, zerovalue=-15.0
+)
+
+# Nicely print metadata
+pprint(metadata_input)
+
+# Estimate the motion field
+V = dense_lucaskanade(R_input)
+
+# Define nowcast keyword arguments
+nowcast_kwargs = {
+    "n_ens_members": n_ens_members,
+    "n_cascade_levels": 6,
+    "R_thr": -10.0,
+    "kmperpixel": 2.0,
+    "timestep": 5,
+    "noise_method": "nonparametric",
+    "vel_pert_method": "bps",
+    "mask_method": "incremental",
+}
+
+# Calculate the STEPS nowcasts
+R_steps = forecast(
+    R_input[-3:, :, :],
+    V,
+    n_timesteps,
+    5,
+    "steps",
+    R_nwp=None,
+    start_blending=start_blending,
+    end_blending=end_blending,
+    use_nwp=True,
+    nowcast_kwargs=nowcast_kwargs,
+)
+
+# Calculate the mean
+R_steps_mean = np.mean(R_steps[:, :, :, :], axis=0)
+
+"""
+# Plot the STEPS nowcasts
+for i in range(n_timesteps):
+    t = date + i * timedelta(minutes=5) + timedelta(minutes=5)
+    print(t)
+    
+    plot_precip_field(R_steps_mean[i, :, :], geodata=geodata_radar)
+    plt.title("STEPS forecast at {}".format(datetime.strftime(t, "%Y-%m-%d %H:%M:%S")))
+    plt.savefig(
+        "./images/5_steps_forecast_{}.png".format(datetime.strftime(t, "%Y%m%d%H%M%S"))
+    )
+    plt.close()
+"""
+
+########################################################################
+# 6. Blending with start=0 and end=end and dummy data
+
+# Downscale the dummy NWP data
+R_dummy, _ = dimension.aggregate_fields_space(
+    R_dummy, metadata_input, metadata_input["xpixelsize"] * 3
+)
+
+# Calculate the blended field
+R_blended_1 = linear_blending(
+    R_steps,
+    n_timesteps,
+    5,
+    R_nwp=R_dummy[1:, :, :],
+    start_blending=0,
+    end_blending=n_timesteps * 5,
+    use_nwp=False,
+)
+
+# Calculate the mean
+R_blended_mean_1 = np.mean(R_blended_1[:, :, :, :], axis=0)
+
+"""
+# Plot the blended fields
+for i in range(n_timesteps):
+    t = date + i * timedelta(minutes=5) + timedelta(minutes=5)
+    print(t)
+    
+    plot_precip_field(R_blended_mean_1[i, :, :], geodata=geodata_radar)
+    plt.title("Blended forecast at {} (version 1)".format(datetime.strftime(t, "%Y-%m-%d %H:%M:%S")))
+    plt.savefig(
+        "./images/6_blended_forecast_version_1_{}.png".format(datetime.strftime(t, "%Y%m%d%H%M%S"))
+    )
+    plt.close()
+"""
+
+########################################################################
+# 7. Blending with start and end and dummy data
+
+# Calculate the blended fields
+R_blended_2 = linear_blending(
+    R_steps,
+    n_timesteps,
+    5,
+    R_nwp=R_dummy[1:, :, :],
+    start_blending=start_blending,
+    end_blending=end_blending,
+)
+
+# Calculate the mean
+R_blended_mean_2 = np.mean(R_blended_2[:, :, :, :], axis=0)
+
+"""
+# Plot the blended fields
+for i in range(n_timesteps):
+    t = date + i * timedelta(minutes=5) + timedelta(minutes=5)
+    print(t)
+    
+    plot_precip_field(R_blended_mean_2[i, :, :], geodata=geodata_radar)
+    plt.title("Blended forecast at {} (version 2)".format(datetime.strftime(t, "%Y-%m-%d %H:%M:%S")))
+    plt.savefig(
+        "./images/7_blended_forecast_version_2_{}.png".format(datetime.strftime(t, "%Y%m%d%H%M%S"))
+    )
+    plt.close()
+"""
+
+########################################################################
+# 8. Blending with start and end and real NWP data
+
+# Downscale the reprojected NWP data
+R_NWP_rprj, _ = dimension.aggregate_fields_space(
+    R_NWP_rprj, metadata_input, metadata_input["xpixelsize"] * 3
+)
+
+# Calculate the blended fields
+R_blended_3 = linear_blending(
+    R_steps,
+    n_timesteps,
+    5,
+    R_nwp=R_NWP_rprj[1:, :, :],
+    start_blending=start_blending,
+    end_blending=end_blending,
+)
+
+# Calculate the mean
+R_blended_mean_3 = np.mean(R_blended_3[:, :, :, :], axis=0)
+
+"""
+# Plot the blended fields
+for i in range(n_timesteps):
+    t = date + i * timedelta(minutes=5) + timedelta(minutes=5)
+    print(t)
+    
+    plot_precip_field(R_blended_mean_3[i, :, :], geodata=geodata_radar)
+    plt.title("Blended forecast at {} (version 3)".format(datetime.strftime(t, "%Y-%m-%d %H:%M:%S")))
+    plt.savefig(
+        "./images/8_blended_forecast_version_3_{}.png".format(datetime.strftime(t, "%Y%m%d%H%M%S"))
+    )
+    plt.close()
+"""
+
+########################################################################
+# 9. Verification
+
+# Convert radar images to np.ndarray
+R_radar = np.array(R_radar[:])
+
+# Downscale radar images
+R_radar, _ = dimension.aggregate_fields_space(
+    R_radar, metadata_input, metadata_input["xpixelsize"] * 3
+)
+
+# Plot rank histogram
+rankhist = verification.rankhist_init(R_blended_2.shape[0], 0.1)
+verification.rankhist_accum(rankhist, R_blended_2[:, -1, :, :], R_radar[-1, :, :])
+fig, ax = plt.subplots()
+verification.plot_rankhist(rankhist, ax)
+ax.set_title("Rank histogram (+%i min)" % (n_timesteps * 5))
+plt.savefig("A.png")
+plt.show()
+
+# Plot rank histogram
+rankhist = verification.rankhist_init(R_blended_3.shape[0], 0.1)
+verification.rankhist_accum(rankhist, R_blended_3[:, -1, :, :], R_radar[-1, :, :])
+fig, ax = plt.subplots()
+verification.plot_rankhist(rankhist, ax)
+ax.set_title("Rank histogram (+%i min)" % (n_timesteps * 5))
+plt.savefig("B.png")
+plt.show()
