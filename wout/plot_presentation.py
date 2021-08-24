4272059301690849ca1dca95105e6d9b2d729529
Wout Dewettinck <wout.dewettinck@ugent.be>
2021-08-24 15:57:02 +0200

    Plot dummy NWP data

--- wout/plot_presentation.py
+++ wout/plot_presentation.py
@@ -13,9 +13,15 @@ from pysteps.visualization import plot_precip_field
 from pysteps.io import import_rmi_nwp_xr
 from pysteps.blending.utils import reprojection
 
-
-# Give date as input
+# Input
 date_str = "202107141000"  # input("Give date and time (e.g.: 201609281600):    ")
+n_timesteps = 36
+start_blending = 60
+end_blending = 120
+
+# 1. Plot radar fields
+
+# Convert date to string
 date = datetime.strptime(date_str, "%Y%m%d%H%M")
 
 # Import the data source
@@ -62,13 +68,18 @@ plt.savefig("./images/1_radar_image_{}.png".format(datetime.strftime(date, "%Y%m
 plt.close()
 """
 
+# Plot the rainfall field
+plt.figure(1)
+plot_precip_field(R_radar, geodata=geodata_radar)
+plt.title("Radar image at {}".format(datetime.strftime(date, "%Y-%m-%d %H:%M:%S")))
+
 # date = date + timedelta(minutes=5)
 
 # Import native NWP data
-date_str = "202107140600"
-date = datetime.strptime(date_str, "%Y%m%d%H%M")
+nwp_date_str = "202107140600"
+nwp_date = datetime.strptime(nwp_date_str, "%Y%m%d%H%M")
 R_NWP = import_rmi_nwp_xr(
-    "./nwp/ao13_{}_native_5min.nc".format(datetime.strftime(date, "%Y%m%d%H"))
+    "./nwp/ao13_{}_native_5min.nc".format(datetime.strftime(nwp_date, "%Y%m%d%H"))
 )
 
 xpixelsize_NWP = R_NWP.attrs["xpixelsize"]
@@ -94,23 +105,80 @@ R_NWP[:], R_NWP.attrs = conversion.to_rainrate(R_NWP[:], R_NWP.attrs)
 t0_str = "202107141005"
 t0 = datetime.strptime(t0_str, "%Y%m%d%H%M")
 
+# Plot reprojected NWP data
+R_NWP_rprj = reprojection(R_NWP[0, :, :], R_radar)
+
+plt.figure(2)
+plot_precip_field(R_NWP[0, :, :], geodata=geodata_NWP)
+plt.title("NWP data at {}".format(datetime.strftime(t0, "%Y-%m-%d %H:%M:%S")))
+
+# Plot reprojected NWP data
+plt.figure(3)
+plot_precip_field(R_NWP_rprj, geodata=geodata_radar)
+plt.title(
+    "Reprojected NWP data at {}".format(datetime.strftime(t0, "%Y-%m-%d %H:%M:%S"))
+)
+
+plt.close()
+
+"""
 for i in range(R_NWP.shape[0]):
     # Plot native NWP data
     t = t0 + i * timedelta(minutes=5)
     print(t)
 
-    """
-	plot_precip_field(R_NWP[i, :, :], geodata=geodata_NWP)
-	plt.title("NWP data at {}".format(datetime.strftime(t, "%Y-%m-%d %H:%M:%S")))
-	plt.savefig("./images/2_nwp_data_{}.png".format(datetime.strftime(t, "%Y%m%d%H%M%S")))
-	plt.close()
+    plot_precip_field(R_NWP[i, :, :], geodata=geodata_NWP)
+    plt.title("NWP data at {}".format(datetime.strftime(t, "%Y-%m-%d %H:%M:%S")))
+    plt.savefig("./images/2_nwp_data_{}.png".format(datetime.strftime(t, "%Y%m%d%H%M%S")))
+    plt.close()
 
-	# Reproject NWP data onto radar domain
-	R_NWP_rprj = reprojection(R_NWP[i, :, :], R_radar)
+    # Reproject NWP data onto radar domain
+    R_NWP_rprj = reprojection(R_NWP[i, :, :], R_radar)
 
-	# Plot reprojected NWP data
-	plot_precip_field(R_NWP_rprj, geodata=geodata_radar)
-	plt.title("Reprojected NWP data at {}".format(datetime.strftime(t, "%Y-%m-%d %H:%M:%S")))
-	plt.savefig("./images/3_reprojected_nwp_data_{}.png".format(datetime.strftime(t, "%Y%m%d%H%M%S")))
-	plt.close()
-	"""
+    # Plot reprojected NWP data
+    plot_precip_field(R_NWP_rprj, geodata=geodata_radar)
+    plt.title("Reprojected NWP data at {}".format(datetime.strftime(t, "%Y-%m-%d %H:%M:%S")))
+    plt.savefig("./images/3_reprojected_nwp_data_{}.png".format(datetime.strftime(t, "%Y%m%d%H%M%S")))
+    plt.close()
+"""
+
+# Plot dummy NWP data
+
+
+def gaussian(x, max, mean, sigma):
+    return max * np.exp(-(x - mean) * (x - mean) / sigma / sigma / 2)
+
+
+def dummy_nwp(R, n_leadtimes, max=15, mean=3, sigma=0.25, speed=100):
+    """Generates dummy NWP data with the same dimension as the input
+    precipitation field R. The NWP data is a vertical line with a Gaussian
+    profile moving to the left"""
+
+    # R is original radar image
+    rows = R.shape[0]
+    cols = R.shape[1]
+
+    # Initialise the dummy NWP data
+    R_nwp = np.zeros((n_leadtimes, rows, cols))
+    x = np.linspace(-5, 5, cols)
+
+    for n in range(n_leadtimes):
+        for i in range(rows):
+            R_nwp[n, i, :] = gaussian(x, max, mean, sigma)
+        mean -= speed / rows
+
+    return R_nwp
+
+
+R_dummy = dummy_nwp(R_radar, n_timesteps)
+
+for i in range(n_timesteps):
+    t = date + timedelta(minutes=5) * (i + 1)
+    print(t)
+
+    plot_precip_field(R_dummy[i, :, :], geodata=geodata_radar)
+    plt.title("Dummy NWP data at {}".format(datetime.strftime(t, "%Y-%m-%d %H:%M:%S")))
+    plt.savefig(
+        "./images/4_dummy_nwp_data_{}.png".format(datetime.strftime(t, "%Y%m%d%H%M%S"))
+    )
+    plt.close()
